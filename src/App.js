a9ee7cf18a1b448cea2647b4ab4b560f81049a0a
sabrizlt <sabrizaltini6@gmail.com>
2023-04-17 15:29:45 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import MyFooter from './components/MyFooter'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
 
 function App() {
   return (
     <div className="App">
-    
+    <MyFooter />
     </div>
   );
 }
