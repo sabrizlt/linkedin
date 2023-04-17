a9a933759549248ae930f218e7604f540b637cdd
davide122 <davidemarchica277@gmail.com>
2023-04-17 15:51:43 +0200

    mycommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FaBeer } from 'react-icons/fa';
+
+
+import { Row } from'react-bootstrap';
+import LateralComponent from './components/LateralComponent';
+import { Col } from'react-bootstrap';
+import { Container } from'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <>
+ <Container>
+ <Row>
+<Col md={8}>
+  lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt lorem ipsum dolor sit amet, consectetur adip
+</Col>
+<Col md={4}>
+<LateralComponent></LateralComponent>
+</Col>
+ </Row>
+ </Container>
+ </>
   );
 }
 
