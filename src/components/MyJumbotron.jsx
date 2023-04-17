099143e1c51011c99cc54f169531467bf53b8a49
manulelee <emanuele.sy@gmail.com>
2023-04-17 13:37:38 +0200

    fine componente jumbotron

---
+++ src/components/MyJumbotron.jsx
@@ -0,0 +1,62 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import { BsPencil } from "react-icons/bs";
+
+const MyJumbotron = () => {
+  return (
+    <Card>
+      <Card.Img
+        variant="top"
+        src="https://placedog.net/500"
+        id="profileCoverImg"
+        alt="Profile cover"
+        style={{ width: "100%" }}
+      />
+      <Card.Body>
+        <Row>
+          <Col xs={3}>
+            <img id="profilePicImg" className="rounded-circle" src="https://placedog.net/500/500" alt="Profile pic" />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-end me-2 fs-3 iconWithHoverEffect">
+            <BsPencil className="rounded-circle p-2 m-5" />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={9} className="justify-content-start">
+            <h2>NOME E COGNOME</h2>
+            <span>posizione lavorativa</span>
+            <br />
+            <small>informazioni di contatto</small>
+          </Col>
+          <Col xs={3}>logo posizione lavorativa</Col>
+        </Row>
+        <Row className="my-2 cardBtns">
+          <Col>
+            <span className="btn btn-primary rounded-pill me-1">Disponibile per:</span>
+            <span className="btn btn-outline-primary rounded-pill me-1">Aggiungi sezione del profilo</span>
+            <span className="btn btn-outline-secondary rounded-pill">Altro</span>
+          </Col>
+        </Row>
+
+        <div className="">
+          <Row className="profileBanner w-75 rounded py-3 ">
+            <Col xs={9} className="ms-2 ">
+              <b>Disponibile a lavorare</b>
+              <br />
+              Ruoli di Sviluppatore Web, Sviluppatore front-end, Sviluppatore back-end e Sviluppatore Javascript
+              <br />
+              <a href="#">Mostra dettagli</a>
+            </Col>
+            <Col xs={2} className=" text-end me-1 rounded-circle p-2 iconWithHoverEffect">
+              <BsPencil className="rounded-circle " />
+            </Col>
+          </Row>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+export default MyJumbotron;
