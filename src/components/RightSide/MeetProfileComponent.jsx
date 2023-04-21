1c6a7ae8cfc6817e8b4d01ca2a1f91fe0343a1a9
Cujba Tiberiu Stefan <123594218+CujbaTiberiu@users.noreply.github.com>
2023-04-21 15:12:28 +0200

    ok

--- src/components/RightSide/MeetProfileComponent.jsx
+++ src/components/RightSide/MeetProfileComponent.jsx
@@ -4,10 +4,9 @@ import { Link } from "react-router-dom";
 
 const MeetProfileComponent = ({ profile }) => {
   return (
-    <Card className="fw-bold mt-3 ">
-      <Card.Body className="pb-0 fs-5"></Card.Body>
-      <Card.Body>
-        <div className="d-flex align-item-center">
+    <Card className="fw-bold mt-3">
+      <Card.Body className="">
+        <div className="d-flex align-item-center flex-md-column align-items-md-center flex-lg-row">
           <img
             className="prof__icon rounded-circle"
             src={profile?.image ? profile?.image : profile.user?.image}
@@ -22,7 +21,7 @@ const MeetProfileComponent = ({ profile }) => {
               <Button
                 variant="outline-secondary"
                 size="sm"
-                className="altroButton rounded-pill px-3"
+                className="altroButton rounded-pill"
               >
                 <BsFillPersonPlusFill /> Collegati
               </Button>
